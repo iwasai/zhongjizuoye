480fb4bfe5ae96b7b6d4827443e9589e90a8a73d
lby3 <1192235026@qq.com>
2017-03-27 19:33:14 +0800

    111

--- app/scripts/controllers/liuyan.js
+++ app/scripts/controllers/liuyan.js
@@ -11,17 +11,35 @@
 angular.module('appApp')
 	.controller('words',['$scope','$http','$state',function($scope,$http,$state){
 //		$scope.username = localStorage.getItem('username')
+		$scope.xingming="";
+		$scope.youxiang="";
+		$scope.dianhua="";
+		$scope.neiron="";
+		$scope.ts=false;
+		$scope.ly=false;
 		$scope.tianjia = function(){
-			$http({
-				url:"http://47.88.16.225:406/users/?id="+localStorage.getItem('ta'),
-				method:"POST",
-				data:{
-					name:$scope.mingzi,
-					content:$scope.neiron
+			if($scope.xingming==""){
+				$scope.ts=true;
+				$scope.hide=function(){
+					$scope.ts=false;
 				}
-			}).then(function(data){
-				console.log(data)
-				$state.go('words')
-			})
+			}else if($scope.neiron==""){
+				$scope.ly=true;
+				$scope.ly_hide=function(){
+					$scope.ly=false;
+				}
+			}else{
+				$http({
+					url:"http://47.88.16.225:406/users/?id="+localStorage.getItem('ta'),
+					method:"POST",
+					data:{
+						name:$scope.xingming,
+						content:$scope.neiron
+					}
+				}).then(function(data){
+					console.log(data)
+					$state.go('words')
+				})
+			}
 		}
 	}])
